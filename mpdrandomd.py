2e9b410d601f4b51ea255630d9eb6139444da979
Mathieu Clabaut <mathieu.clabaut@gmail.com>
2015-08-11 17:25:53 +0200

    Do not used listall for mopidy compatibility
    Plus some minor bug.
    Remove tabulations

--- mpdrandomd.py
+++ mpdrandomd.py
@@ -18,60 +18,60 @@ parser = OptionParser( version="0.1",
         "\n\n\tFeed an mpd daemon with a randomize playlist")
 parser.add_option(  "-k", "--keep",
                     action="store", type="int", dest="keep", 
-		    default=1000,
-		    metavar="NB",
+                    default=1000,
+                    metavar="NB",
                     help="how many songs already played are keeped")
 parser.add_option(  "-n", "--enqueue",
                     action="store", type="int", dest="enqueue", 
-		    default=2,
-		    metavar="NB",
+                    default=2,
+                    metavar="NB",
                     help="how many songs to enqueue before the one playing")
 parser.add_option(  "--daemon",
                     action="store_const", const=True, dest="daemon", 
-		    default=False,
+                    default=False,
                     help="output debug information to stderr")
 parser.add_option(  "--no-update",
                     action="store_const", const=False, dest="update", 
-		    default=True,
+                    default=True,
                     help="do not update the database on startup")
 parser.add_option(  "--clear",
                     action="store_const", const=True, dest="clear", 
-		    default=False,
+                    default=False,
                     help="emtpy mpd database on startup")
 parser.add_option(  "-x", "--exclude-regex",
                     action="append", type="string", dest="exclude", 
-		    default=[],
-		    metavar="REGEX",
+                    default=[],
+                    metavar="REGEX",
                     help="Regex against which matching files will be excluded")
 parser.add_option(  "-e", "--no-equi-album",
                     action="store_const", const=True, dest="equiproba", 
-		    default=False,
+                    default=False,
                     help="Does no correct the probability for album wrt individual songs")
 password, host = mpdclient2.parse_host(os.environ.get('MPD_HOST', 'localhost'))
 parser.add_option(  "-H", "--host",
                     action="store", type="string", dest="host", 
-		    default=host,
-		    metavar="NAME",
+                    default=host,
+                    metavar="NAME",
                     help="MPD host")
 parser.add_option(  "-P", "--port",
                     action="store", type="int", dest="port", 
-		    default=int(os.environ.get('MPD_PORT', 6600)),
-		    metavar="NB",
+                    default=int(os.environ.get('MPD_PORT', 6600)),
+                    metavar="NB",
                     help="MPD port")
 parser.add_option(   "--password",
                     action="store", type="string", dest="password", 
-		    default=password,
-		    metavar="PWD",
+                    default=password,
+                    metavar="PWD",
                     help="MPD connexion password")
 parser.add_option(  "--music-dir",
                     action="store", type="string", dest="musicdir", 
-		    default=None,
-		    metavar="DIR",
+                    default=None,
+                    metavar="DIR",
                     help="MPD music directory (default from mpd.conf)")
 parser.add_option(  "-c","--mpd-conf",
                     action="store", type="string", dest="mpdconf", 
-		    default="/etc/mpd.conf",
-		    metavar="FILE",
+                    default="/etc/mpd.conf",
+                    metavar="FILE",
                     help="MPD conf file (default /etc/mpd.conf")
 parser.set_defaults(verbose=logging.WARNING)
 parser.add_option("-q", "--quiet",
@@ -85,8 +85,8 @@ parser.add_option("-d", "--debug",
                     help="output debug information to stderr")
 parser.add_option(  "-l","--log-file",
                     action="store", type="string", dest="logfile", 
-		    default=LOG_FILENAME,
-		    metavar="FILE",
+                    default=LOG_FILENAME,
+                    metavar="FILE",
                     help="log file (default %s, '-' for stderr)" % LOG_FILENAME)
 
 
@@ -95,9 +95,9 @@ if has_optcomplete:
 
 def loggerInit(opt):
     if opt.logfile == '-':
-	logging.basicConfig(level=opt.verbose,format="%(levelname)-8s %(message)s")
+        logging.basicConfig(level=opt.verbose,format="%(levelname)-8s %(message)s")
     else:
-	logging.basicConfig(filename = opt.logfile, level=opt.verbose,format="%(asctime)s-%(levelname)-8s %(message)s")
+        logging.basicConfig(filename = opt.logfile, level=opt.verbose,format="%(asctime)s-%(levelname)-8s %(message)s")
 
 def Print(verb, str):
     """Print 'str' to stderr if 'verb' is below the current verbosity level"""
@@ -107,64 +107,67 @@ def Print(verb, str):
 
 class RetryMPDConnection(mpdclient2.mpd_connection):
     def __init__(self,host,port,password):
-	self.host=host
-	self.port=port
-	self.password=password
-	mpdclient2.mpd_connection.__init__(self, host, port)
-	if password != '':
-	    self.password(password)
+        self.host=host
+        self.port=port
+        self.password=password
+        mpdclient2.mpd_connection.__init__(self, host, port)
+        if password != '':
+            self.password(password)
 
 
 class RandomPlayList():
     def __init__(self, nb_keeped=5, nb_queued=2, doupdate=True, doclear=False,  host='localhost', port=6600, passwd='', musicdir="/home/music", mpdconf="/etc/mpd.conf", exclude=[], equiproba=True):
-	self.mpdconf = mpdconf
-	self.musicdir=self.init_music_dir(musicdir)
-	logging.debug("Exclude regex : %s" % '|'.join(exclude))
-	self.path_re=re.compile(r'(.*)/[^/]*')
-	self.nb_keeped = nb_keeped
-	self.nb_queued = nb_queued
-	self.doclear = doclear
-	self.c = self.connect(host, port, passwd)
-	if doupdate:
-	    self.update_db()
+        self.mpdconf = mpdconf
+        self.musicdir=self.init_music_dir(musicdir)
+        logging.debug("Exclude regex : %s" % '|'.join(exclude))
+        self.path_re=re.compile(r'(.*)/[^/]*')
+        self.nb_keeped = nb_keeped
+        self.nb_queued = nb_queued
+        self.doclear = doclear
+        self.c = self.connect(host, port, passwd)
+        if doupdate:
+            self.update_db()
         if len(exclude)>0:
             self.except_re=re.compile('|'.join(exclude))
         else:
             self.except_re=re.compile(r'^$')
 
-	#r'Enfants/|soirees/|\.m3u')
+        #r'Enfants/|soirees/|\.m3u')
 
-	self.c.random(0)
+        self.c.random(0)
         logging.debug("RandomPlayList initialized")
-	#self.c.play()
+        #self.c.play()
 
     def init_music_dir(self, musicdir):
-	if musicdir is None:
-	    re_musicdir=re.compile(r"""^\s*music_directory\s*["']([^"']*)["']""")
-	    res=None
-	    with open(self.mpdconf, 'r') as f: 
-		for line in f:
-		    res=re_musicdir.match(line)
-		    if res:
-			break
-	    if res is not None:
-		res = res.group(1)
-		logging.debug("Music directory %s (from %s)" % (res,self.mpdconf))
-		return  res
-	else:
-	    raise RuntimeError("No music directory specified or found")
+        if musicdir is None:
+            re_musicdir=re.compile(r"""^\s*music_directory\s*["']([^"']*)["']""")
+            res=None
+            with open(self.mpdconf, 'r') as f: 
+                for line in f:
+                    res=re_musicdir.match(line)
+                    if res:
+                        break
+            if res is not None:
+                res = res.group(1)
+                logging.debug("Music directory %s (from %s)" % (res,self.mpdconf))
+                return  res
+            else:
+                raise RuntimeError("No music directory specified or found")
+        else:
+            return musicdir
+
 
     def connect(self,  host, port, passwd):
-	c = None
-	while c is None:
-	    try:
-		c = RetryMPDConnection(host,port,passwd)
-	    except Exception,e:
-		logging.info("Failed to connect to mpd")
-		logging.debug(e)
-		c = None
-		time.sleep(3)
-	return c
+        c = None
+        while c is None:
+            try:
+                c = RetryMPDConnection(host,port,passwd)
+            except Exception,e:
+                logging.info("Failed to connect to mpd")
+                logging.debug(e)
+                c = None
+                time.sleep(3)
+        return c
 
 
     def album_path_if_to_be_played_at_once(self, file):
@@ -172,42 +175,42 @@ class RandomPlayList():
         continuously (if a file called "norandom" exists in the album
         directory)
         """
-	found=False
-	while '/' in file and not found:
-	    file=self.path_re.match(file).group(1)
-	    norandompath = "%s/%s/%s" % (self.musicdir,file,"norandom")
-	    found = os.path.lexists(norandompath)
-	if found:
-	    return file
-	else:
-	    return None
+        found=False
+        while '/' in file and not found:
+            file=self.path_re.match(file).group(1)
+            norandompath = "%s/%s/%s" % (self.musicdir,file,"norandom")
+            found = os.path.lexists(norandompath)
+        if found:
+            return file
+        else:
+            return None
 
     def update_db(self):
-	self.c.update()
-	logging.info( "Updating db",)
-	while 'updating_db' in self.c.status():
-	    time.sleep(1)
-	logging.info( "Done")
+        self.c.update()
+        logging.info( "Updating db",)
+        while 'updating_db' in self.c.status():
+            time.sleep(1)
+        logging.info( "Done")
 
     def delete_old_songs(self, pos):
         """ Remove first playlist songs if playlist size is greater than
         nb_keeped"""
-	if pos > self.nb_keeped:
-	    logging.debug("Delete first %d songs" % (pos - self.nb_keeped))
-	    for i in xrange(pos - self.nb_keeped, 0, -1):
-		self.c.delete(i)
+        if pos > self.nb_keeped:
+            logging.debug("Delete first %d songs" % (pos - self.nb_keeped))
+            for i in xrange(pos - self.nb_keeped, 0, -1):
+                self.c.delete(i)
 
 
     def enqueue_new_songs(self, pos, length):
         """Enqueue enough songs so that there is at least nb_queued songs to be
         played ahead.  """
-	if length - pos > self.nb_queued:
-	    return
-	to_be_added = self.nb_queued - length + pos + 1
-	added = 0
-	while  added < to_be_added:
-	    added += self.enqueue_one_song_or_album()
-	logging.debug("Enqued %d songs (%d required)" % (added,to_be_added))
+        if length - pos > self.nb_queued:
+            return
+        to_be_added = self.nb_queued - length + pos + 1
+        added = 0
+        while  added < to_be_added:
+            added += self.enqueue_one_song_or_album()
+        logging.debug("Enqued %d songs (%d required)" % (added,to_be_added))
 
 
     def get_next_random_song_filename(self):
@@ -216,20 +219,20 @@ class RandomPlayList():
         'exclude' regexp.
         :returns: chosen file name
         """
-	pl_files = [ x.file for x in self.c.playlistinfo() if 'file' in x]
-	nb_songs=len(self.songs)
-	chosen_id = random.randrange(0,nb_songs)
-	chosen_file = self.songs[chosen_id].file
-	# get another song if the random one is in the current playlist or
-	# match except_re
-	while (chosen_file in pl_files or self.except_re.match(chosen_file)):
+        pl_files = [ x.file for x in self.c.playlistinfo() if 'file' in x]
+        nb_songs=len(self.songs)
+        chosen_id = random.randrange(0,nb_songs)
+        chosen_file = self.songs[chosen_id].file
+        # get another song if the random one is in the current playlist or
+        # match except_re
+        while (chosen_file in pl_files or self.except_re.search(chosen_file)):
 
-	    if self.except_re.match(chosen_file):
-		self.songs.pop(chosen_id)
+            if self.except_re.search(chosen_file):
+                self.songs.pop(chosen_id)
                 nb_songs-=1
-	    chosen_id = random.randrange(0,nb_songs)
+            chosen_id = random.randrange(0,nb_songs)
             chosen_file = self.songs[chosen_id].file
-	return chosen_file
+        return chosen_file
     
     def skip_album_by_probablity(self, album_len):
         """Each album as album_len times more chance to pick up than stand
@@ -250,57 +253,58 @@ class RandomPlayList():
     def enqueue_one_song_or_album(self):
         """Enqueue a song or the song album if the song belongs to an albow
         marked as 'must not be played randomly'"""
-	file = self.get_next_random_song_filename()
-	if '/' in file:
-	    path = self.album_path_if_to_be_played_at_once(file)
-	    if path is not None:
-		logging.debug("No Random : "+path )
-		album = filter(lambda x: x.file.find(path)==0, self.songs)
+        file = self.get_next_random_song_filename()
+        if '/' in file:
+            path = self.album_path_if_to_be_played_at_once(file)
+            if path is not None:
+                logging.debug("No Random : "+path )
+                album = filter(lambda x: x.file.find(path)==0, self.songs)
                 album_len=len(album)
                 if self.skip_album_by_probablity(album_len):
                     return self.enqueue_one_song_or_album()
-		album.sort(key=lambda x:x.file)
-		logging.debug ("Enqueue one album : %s" % path)
-		for s in album:
-		    self.c.add(s.file)
-		return album_len
-	    else:
-		logging.debug("Enqueue one song : %s" % file)
-		self.c.add(file)
-		return 1
-	else:
-	    logging.debug("Enqueue one song : %s" % file)
-	    self.c.add(file)
-	    return 1
+                album.sort(key=lambda x:x.file)
+                logging.debug ("Enqueue one album : %s" % path)
+                for s in album:
+                    self.c.add(s.file)
+                return album_len
+            else:
+                logging.debug("Enqueue one song : %s" % file)
+                self.c.add(file)
+                return 1
+        else:
+            logging.debug("Enqueue one song : %s" % file)
+            self.c.add(file)
+            return 1
 
 
 
     def feed_mpd(self, sleep_time=3):
         """Feed continuously mpd with new random songs.
         Wait sleep_time between two calls to enqueue_new_songs."""
-	if self.doclear:
-	    self.c.clear()
-	self.songs = filter(lambda x: 'file' in x, self.c.listallinfo())
+        if self.doclear:
+            self.c.clear()
+        #self.songs = filter(lambda x: 'file' in x, self.c.listallinfo())
+        self.songs = self.c.search("file", "local:")
 
-	wasplaying = self.c.status().state == 'play'
+        wasplaying = self.c.status().state == 'play'
 
-	while True:
-	    playlist_len = int(self.c.status().playlistlength)
-	    if playlist_len == 0:
-		self.enqueue_new_songs(0,0)
-		playlist_len = int(self.c.status().playlistlength)
-	    if 'song' in  self.c.status():
-		pos = int(self.c.status().song)
-	    else:
-		pos = 0
-	    self.delete_old_songs(pos)
-	    self.enqueue_new_songs(pos,playlist_len)
-	    #curlen = int(self.c.currentsong().time)
-	    #curtime = int(self.c.status().time.split(':')[0])
-	    #sleep(curlen-curtime + 3)
-	    time.sleep(sleep_time)
+        while True:
+            playlist_len = int(self.c.status().playlistlength)
+            if playlist_len == 0:
+                self.enqueue_new_songs(0,0)
+                playlist_len = int(self.c.status().playlistlength)
+            if 'song' in  self.c.status():
+                pos = int(self.c.status().song)
+            else:
+                pos = 0
+            self.delete_old_songs(pos)
+            self.enqueue_new_songs(pos,playlist_len)
+            #curlen = int(self.c.currentsong().time)
+            #curtime = int(self.c.status().time.split(':')[0])
+            #sleep(curlen-curtime + 3)
+            time.sleep(sleep_time)
 
-		
+                
 
 
 
@@ -314,20 +318,20 @@ def main(argv=None):
         argv = sys.argv
     (options, args) = parser.parse_args()
     if options.daemon:
-	daemon.createDaemon()
+        daemon.createDaemon()
     loggerInit(options)
     while True:
-	try:
+        try:
             logging.debug("Creating RandomPlayList")
-	    r=RandomPlayList(doupdate=options.update, doclear=options.clear,
-		    nb_keeped=options.keep, nb_queued=options.enqueue,
-		    host=options.host, passwd=options.password,
-		    port=options.port, mpdconf=options.mpdconf,
-		    musicdir=options.musicdir, exclude=options.exclude,
+            r=RandomPlayList(doupdate=options.update, doclear=options.clear,
+                    nb_keeped=options.keep, nb_queued=options.enqueue,
+                    host=options.host, passwd=options.password,
+                    port=options.port, mpdconf=options.mpdconf,
+                    musicdir=options.musicdir, exclude=options.exclude,
                     equiproba=options.equiproba)
-	    r.feed_mpd()
-	except Exception,e:
-	    logging.debug(e)
+            r.feed_mpd()
+        except Exception,e:
+            logging.debug(e)
 
 if __name__ == "__main__":
     sys.exit(main())
